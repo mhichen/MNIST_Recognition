d48fd18b5f7b6e7e5be71ae7e801535df843da1d
Min-Hua Ivy Chen <mhichen@umich.edu>
2018-05-24 06:55:23 -0500

    Another attempt at LeNet-5.

--- digit_classification_NN.py
+++ digit_classification_NN.py
@@ -22,7 +22,14 @@ def show_image(pixels, labels, ind, shape):
     plt.title(int(labels[ind]))
     plt.axis("off")
     plt.show()
-        
+    
+def fetch_batch(epoch, batch_index, batch_size):
+    np.random.seed(epoch * n_batches + batch_index)  
+    indices = np.random.randint(m, size=batch_size)  
+    X_batch = scaled_housing_data_plus_bias[indices] 
+    y_batch = housing.target.reshape(-1, 1)[indices] 
+    return X_batch, y_batch
+    
 if __name__ == "__main__":
 
     now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
@@ -51,7 +58,6 @@ if __name__ == "__main__":
     
     print("test has dimensions", X.shape)
     
-    m = X.shape[0]
     
     #Y_mod = label_binarize(Y, classes = range(10))
 
@@ -66,157 +72,144 @@ if __name__ == "__main__":
     ## Further split train into validation
     X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 5000, random_state = 9012, shuffle = True)
 
+
+    ## For debugging only
+    # X_train = X_train[0:200, :, :, :]
+    # Y_train = Y_train[0:200]
+
     m_train = X_train.shape[0]
     m_val = X_val.shape[0]
     m_test = X_test.shape[0]
+
+    n_inputs = X.shape[0]
+    n_outputs = 10
+    n_epochs = 200
+    batch_size = 10
     
+    learning_rate = 0.01
+        
     print("m_train", m_train)
     print("m_val", m_val)
     print("m_test", m_test)
     
 
-    ## For debugging only
-    # X_train = X_train[0:200]
-    # Y_train = Y_train[0:200]
-
-
-    n_inputs = X.shape[0]
-    # n_hidden1 = 200
-    # n_hidden2 = 200
-    n_outputs = 10
-
-    # #for n_hidden1, n_hidden2 in zip(n_hidden1_list, n_hidden2_list):
-
-    # #tf.reset_default_graph()
-        
     X = tf.placeholder(tf.float32, shape = (None, 32, 32, 1), name = "X")
     Y = tf.placeholder(tf.int64, shape = (None), name = "Y")
 
     train_writer = tf.summary.FileWriter(logdir + '/train', tf.get_default_graph())
     val_writer = tf.summary.FileWriter(logdir + '/val', tf.get_default_graph())
 
-    # filters = np.zeros(shape = (), dtype = np.float32)
-    
     with tf.name_scope("LeNet-5"):
 
         # C1 - Convolution
         C1 = tf.layers.conv2d(X, filters = 6, kernel_size = [5, 5], strides = (1, 1),
                                    padding = 'valid', activation = tf.nn.tanh)
 
+        # S2 - Average Pooling
         S2 = tf.layers.average_pooling2d(C1, pool_size = [2, 2], strides = (2, 2),
                                          padding = 'valid')
 
+        # C3 - Convolution
         C3 = tf.layers.conv2d(S2, filters = 16, kernel_size = [5, 5], strides = (1, 1),
                                    padding = 'valid', activation = tf.nn.tanh)
 
+        # S4 - Average Pooling
         S4 = tf.layers.average_pooling2d(C3, pool_size = [2, 2], strides = (2, 2),
                                          padding = 'valid')
 
-        C5 = tf.layers.conv2d(S4, filters = 120, kernel_size = [5, 5], strides = (1, 1),
+        # C5 - Convolution
+        C5 = tf.layers.conv2d(S4, filters = 1, kernel_size = [5, 5], strides = (1, 1),
                                    padding = 'valid', activation = tf.nn.tanh)
-
+        
         F6 = tf.layers.dense(C5, units = 120, activation = tf.nn.tanh)
 
-        logits = tf.layers.dense(F6, units = n_outputs)
+        F7 = tf.layers.dense(F6, units = 84, activation = tf.nn.tanh)
+
+        logits = tf.layers.dense(F7, units = n_outputs)
 
         logits = tf.layers.flatten(logits)
-        
+
         tf.summary.histogram('C1', C1)
 
-        # hidden2 = tf.layers.dense(hidden1, n_hidden2, name = "hidden2",
-        #                           activation = tf.nn.leaky_relu)
-
         tf.summary.histogram('S2', S2)
-        
-        # logits = tf.layers.dense(hidden2, n_outputs, name = "outputs")
-        
 
     with tf.name_scope("loss"):
 
-
         loss = tf.losses.sparse_softmax_cross_entropy(labels = Y, logits = logits)
-
-        #loss = tf.reduce_mean(cross_entropy, name = "loss")
-        
-        # cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = Y,
-        #                                                                logits = logits)
-                
-        # loss = tf.reduce_mean(cross_entropy, name = "loss")
-
-    learning_rate = 0.01
 
     with tf.name_scope("train"):
 
         optimizer = tf.train.GradientDescentOptimizer(learning_rate)
         train_op = optimizer.minimize(loss)
         
-    #     # optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    #     optimizer = tf.train.AdamOptimizer(learning_rate)
-
-    #     training_op = optimizer.minimize(loss)
-                
     with tf.name_scope("eval"):
 
-    #     #correct = tf.nn.in_top_k(logits, Y, 1)
-    #     #accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+        correct = tf.nn.in_top_k(logits, Y, 1)
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
             
         prediction = tf.argmax(logits, axis = 1)
         precision, precision_op = tf.metrics.precision(Y, tf.argmax(logits, axis = 1))
         recall, recall_op = tf.metrics.recall(Y, prediction)
 
-
+        tf.summary.scalar('accuracy', accuracy)
         tf.summary.scalar('precision', precision)
         tf.summary.scalar('recall', recall)
 
         write_op = tf.summary.merge_all()
-        
-
 
     init = tf.global_variables_initializer()
     init_local = tf.local_variables_initializer()
 
-    # saver = tf.train.Saver
     
-    n_epochs = 200
-    # #batch_size = 10
-
     with tf.Session() as sess:
 
-    #     print()
-    #     print("Starting to train with Leaky ReLu activation and Adam")
-    #     print("First hidden layer:", n_hidden1)
-    #     print("Second hidden layer:", n_hidden2)
-
-            
-    #     start_time = time.time()
+        start_time = time.time()
             
         init.run()
         init_local.run()
-            
+
+        n_batches = int(m_train/batch_size)
+
+        print("n_batches", n_batches)
+        print()
+        
         for epoch in range(n_epochs):
 
-            print("Epoch", epoch)
-            sess.run(train_op, feed_dict = {X: X_train, Y: Y_train})
+            for b in range(n_batches):
 
+                # print("batch b", b, b*batch_size, (b + 1)*batch_size)
 
-            prec_train = sess.run(precision_op, feed_dict = {X: X_train, Y: Y_train})
-            recall_train = sess.run(recall_op, feed_dict = {X: X_train, Y: Y_train})
+                if (b - 1) != n_batches:
+                    X_batch = X_train[b*batch_size:(b + 1)*batch_size, :, :, :]
+                    Y_batch = Y_train[b*batch_size:(b + 1)*batch_size]
+
+                else:
+                    X_batch = X_train[b*batch_size:, :, :, :]
+                    Y_batch = Y_train[b*batch_size:]
+
+                # print("X_batch", X_batch.shape)
+                # print("Y_batch", Y_batch.shape)
+                
+                sess.run(train_op, feed_dict = {X: X_batch, Y: Y_batch})
+
+                _, prec_train, recall_train, accuracy_train = sess.run([train_op, precision_op, recall_op, accuracy], feed_dict = {X: X_batch, Y: Y_batch})
+                #recall_train = sess.run(recall_op, feed_dict = {X: X_batch, Y: Y_batch})
             
-            prec_val = sess.run(precision_op, feed_dict = {X: X_val, Y: Y_val})
-            recall_val = sess.run(recall_op, feed_dict = {X: X_val, Y: Y_val})
+                train = write_op.eval(feed_dict = {X: X_batch, Y: Y_batch})
+                train_writer.add_summary(train, epoch)
+                
+            prec_val, recall_val, accuracy_val = sess.run([precision_op, recall_op, accuracy], feed_dict = {X: X_val, Y: Y_val})
+            #recall_val = sess.run(recall_op, feed_dict = {X: X_val, Y: Y_val})
 
-            train = write_op.eval(feed_dict = {X: X_train, Y: Y_train})
-            train_writer.add_summary(train, epoch)
-            
             validation = write_op.eval(feed_dict = {X: X_val, Y: Y_val})
             val_writer.add_summary(validation, epoch)
             
-            print("Epoch ", epoch, "Train precision:", prec_train, "Train recall:", recall_train)
-            print("Epoch:", epoch, "Val precision:", prec_val, "Val recall:", recall_val)
+            print("Epoch:", epoch, "Train precision:", prec_train, "Train recall:", recall_train, "Train accuracy:", accuracy_train)
+            print("Epoch:", epoch, "Val precision:", prec_val, "Val recall:", recall_val, "Val accuracy:", accuracy_val)
             print()
 
     train_writer.close()
     val_writer.close()
     
-    # print("Time elapsed", (time.time() - start_time)/60, "minutes")
+    print("Time elapsed", (time.time() - start_time)/60, "minutes")
     # print()
